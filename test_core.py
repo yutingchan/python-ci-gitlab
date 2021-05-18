1bb5c4ea8aec01cbf46024d67500d07c6011b3a1
yutingchan <49631189+yutingchan@users.noreply.github.com>
2021-05-18 23:17:11 +0800

    Add files via upload

---
+++ test_core.py
@@ -0,0 +1,41 @@
+import pytest
+import requests
+
+from cathay.sample.customer import Customer
+from cathay.sample.core import CustomerDataProcess
+from decimal import Decimal, ROUND_DOWN
+
+INIT_MONEY=100.0
+
+class TestCoreSuites:
+##########################################################################################
+#
+# 假設這位客戶, 名字是 Test User, 帳號為100-1100, 一開始帳戶會先存100元, 要測試下面項目: 
+    test_user = Customer('Test User', '100-1100')
+    test_user.deposit(INIT_MONEY)
+#
+# 1. 之後存款 1000 元, 確認帳戶總金額為 1100 元
+    test_user.deposit(1000)
+    assert (test_user.balance == 1100)
+
+# 2. 下一步提款 500 元, 確認帳戶總金額為 600 元
+    test_user.withdraw(500)
+    assert (test_user.balance == 600)
+
+# 3. 假設銀行年利率是10%, 經過一年之後確認帳戶餘額為660元
+    CustomerDataProcess().add_interest(test_user, 0.1)
+    assert (int(test_user.balance) == 660)
+
+# 4.之後提款 700 元, pytest 預期會接到 RuntimeError
+    test_user.withdraw(700)
+
+# 第二題
+#1.
+    response = requests.get('https://cathay-ds-test.s3-ap-northeast-1.amazonaws.com/user_info?userid=A123456789')
+# 伺服器回應的狀態碼
+    assert response.status_code == 200
+#2.
+    r = requests.get('https://cathay-ds-test.s3-ap-northeast-1.amazonaws.com/company_info?companyid=1')
+    assert response.status_code == 200
+
+##########################################################################################
